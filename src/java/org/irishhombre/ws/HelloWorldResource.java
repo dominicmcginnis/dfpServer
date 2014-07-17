175613dc9b74773dae0143e83a59928bb529cce4
Dominic McGinnis <dominicmcginnis@gmail.com>
2014-07-17 10:09:45 -0700

    adding in simple RESTful service config and test

---
+++ src/java/org/irishhombre/ws/HelloWorldResource.java
@@ -0,0 +1,12 @@
+package org.irishhombre.ws.*;
+import javax.ws.rs.*;
+
+@Path("helloworld")
+public class HelloWorldResource {
+    @GET
+    @Produces("text/plain")
+    public String getMessage() {
+        return "Rest Never Sleeps";
+    }
+}
+
